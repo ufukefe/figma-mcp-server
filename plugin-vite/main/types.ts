96a74add5950fe5a6bfad3fc4b047325ef518b76
Antonytm <antontishchenko@gmail.com>
2025-11-07 20:21:44 +0200

    wip: migration from plugin

---
+++ plugin-vite/main/types.ts
@@ -0,0 +1,18 @@
+import { EventHandler } from '@create-figma-plugin/utilities'
+import { FromPluginMessage } from '@shared/types'
+
+
+export interface StartTaskHandler extends EventHandler {
+  name: 'START_TASK'
+  taskId: string
+  command: string
+  args: any
+}
+
+export interface TaskFinishedHandler extends EventHandler, FromPluginMessage {
+  name: 'TASK_FINISHED'
+}
+
+export interface TaskFailedHandler extends EventHandler, FromPluginMessage {
+  name: 'TASK_FAILED'
+}
